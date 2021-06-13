821631398750519b9765a1160b9b6da3c40243bf
m-bansal <67611924+m-bansal@users.noreply.github.com>
2021-06-13 11:10:23 +0530

    Update selection sort.cpp

--- selection sort.cpp
+++ selection sort.cpp
@@ -4,15 +4,17 @@ void swap(int *x, int *y){
     temp = *x;
     *x = *y;
     *y = temp;
-}
+} //function to swap the elements of the array for sorting
+//moving boundary of unsorted subarray one by one
 void ss(int A[], int n){
     int Min_index, i, j;
     for(i=0; i<n-1; i++){
+	//finding the minimum element in unsorted array
         Min_index = i;
         for(j=i+1; j<n; j++){
             if(A[j]< A[Min_index])
                 Min_index=j;
-            swap(&A[i], &A[Min_index]);
+            swap(&A[i], &A[Min_index]);//swapping the found minimum element with the first element
         }
     }
 }
@@ -20,6 +22,7 @@ int main() {
 	int B[] = {21, 64, 76, 12, 67, 32};
 	int n = sizeof(B)/sizeof(B[0]);
 	ss(B, n);
+	//print array
 	for(int i=0; i<n; i++)
 	    printf("%d ", B[i]);
 	return 0;
